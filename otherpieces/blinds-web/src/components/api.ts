0ca1e0f1d27d5e9f23b7948e9bc7425114616a99
Gang Zhang <gzhangx@hotmail.com>
2024-02-15 00:37:24 -0500

    page

---
+++ otherpieces/blinds-web/src/components/api.ts
@@ -0,0 +1,25 @@
+const url = "http://192.168.0.40:18082";
+
+import { util } from '@gzhangx/googleapi'
+
+
+
+export type ControlTypes = 'servo';
+export interface ControlSwitch {
+    name: string;
+    ctlType: ControlTypes;
+}
+export interface ShutterObj {
+    name: string;
+    ip: string;
+    updateTime: string;
+    controls: ControlSwitch[];
+}
+export async function getBlinds() {
+    const res = await util.doHttpRequest({
+        method: 'GET',
+        url: `${url}/getBlinds`,
+    });
+    console.log(res);
+    return res as any as { data: ShutterObj };
+}
