c557994368146566b117d56514a4489a16425737
Gang Zhang <gzhangx@hotmail.com>
2024-02-23 16:54:56 -0500

    fix

--- otherpieces/blinds-web/src/app/page.tsx
+++ otherpieces/blinds-web/src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
                     {c.controls.map(cc => cc.name).join(',')}
                     <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                       {c.controls.map((cc, ctli) => {
-                        const value = cc.value || '-1';
+                        const value = cc.value || '';
                         return <Fragment key={`shutter_cctl_r${keyc}_ctl_${ctli}`} ><div key={`shutter_cctl_${keyc}_ctl_${ctli}`}>
                           Ctrl:{cc.name} <input type="text" value={value} onChange={e => {                            
                             cc.value = e.target.value;
